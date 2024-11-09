eb4bf4a855a35428a6c4b09eed3c066ffa0bc80a
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-11-09 18:21:46 -0500

    Introduced BaseModel for the other models to inherit from

--- backend/RepoData/models.py
+++ backend/RepoData/models.py
@@ -2,24 +2,31 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    etag = models.CharField(max_length=20)
+# Base model for all the models
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+# User model to store github username and etag associated to data received from Github API
+class User(BaseModel):
+    username = models.CharField(max_length=50, unique=True)
+    etag = models.CharField(max_length=20)
+
     def __str__(self):
-        return self.username
+        return str(self.username)
 
 
-class Repository(models.Model):
+# Repository model which stores all the repositories related to different users in the User table
+class Repository(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     repository_name = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
     stars = models.IntegerField(default=0)
     forks = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.repository_name) + " belonging to " + str(self.user)
