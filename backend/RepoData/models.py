aef3c61a7a8c85c5b821c3d8b0d098d31cb72d19
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-11-10 12:34:23 -0500

    Added etag and pagenumber to repository table to track versions of data

--- backend/RepoData/models.py
+++ backend/RepoData/models.py
@@ -25,8 +25,15 @@ class Repository(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     repository_name = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
+    page_number = models.IntegerField(default=1)
+    etag = models.CharField(max_length=30, default="")
     stars = models.IntegerField(default=0)
     forks = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'repository_name'], name='unique_user_repository')
+        ]
 
     def __str__(self):
         return str(self.repository_name) + " belonging to " + str(self.user)
