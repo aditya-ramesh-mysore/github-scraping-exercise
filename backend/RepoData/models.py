1b49baa4bcec002508344aadff25bbb2eb8adbd9
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-11-14 13:07:56 -0500

    Modified description and repository name maxlength

--- backend/RepoData/models.py
+++ backend/RepoData/models.py
@@ -13,7 +13,7 @@ class BaseModel(models.Model):
 
 # User model to store github username and etag associated to data received from Github API
 class User(BaseModel):
-    username = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=150, unique=True)
 
     def __str__(self):
         return str(self.username)
@@ -22,8 +22,8 @@ class User(BaseModel):
 # Repository model which stores all the repositories related to different users in the User table
 class Repository(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    repository_name = models.CharField(max_length=100)
-    description = models.CharField(max_length=400, null=True)
+    repository_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000, null=True)
     page_number = models.IntegerField(default=1)
     etag = models.CharField(max_length=100, default="")
     stars = models.IntegerField(default=0)
