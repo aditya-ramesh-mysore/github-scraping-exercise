c9e66d79c2d02060376a9391b121e8a15fd41a18
aditya-ramesh-mysore <mysore.ad@northeastern.edu>
2024-11-13 20:53:54 -0500

    Done with Repository view testing

--- backend/RepoData/tests/test_repository_view.py
+++ backend/RepoData/tests/test_repository_view.py
@@ -1,55 +1,52 @@
 from django.test import TestCase, Client
-from RepoData.models import *
 from rest_framework import status
 
-class UserViewTest(TestCase):
+from ..models import Repository, User
 
+
+class RepositoryViewTest(TestCase):
 
     def setUp(self):
-        User.objects.create(username='testuser1')
-        User.objects.create(username='testuser2')
-        User.objects.create(username='testuser3')
+        self.user = User.objects.create(username='testuser')
+        Repository.objects.create(user=self.user, repository_name='repo1', stars=100)
+        Repository.objects.create(user=self.user, repository_name='repo2', stars=50)
+        Repository.objects.create(user=self.user, repository_name='repo3', stars=150)
         self.client = Client()
-        self.url = "http://127.0.0.1:8000/v1/users/"
+        self.url = 'http://127.0.0.1:8000/v1/repositories/'
 
-    def test_get_recent_users_success(self):
+    def test_get_most_starred_repositories_success(self):
         response = self.client.get(self.url, {'recent': 2, 'page': 1})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
-        data = response.data
-        self.assertEqual(data[0]['username'], 'testuser3')
-        self.assertEqual(data[1]['username'], 'testuser2')
 
-    def test_get_recent_users_default(self):
+    def test_get_most_starred_repositories_default(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
 
-    def test_get_recent_users_success_pagination(self):
+    def test_get_most_starred_repositories_success_pagination(self):
         for i in range(4, 14):
-            User.objects.create(username='testuser' + str(i))
+            Repository.objects.create(user=self.user, repository_name='repo' + str(i), stars=10)
 
         response = self.client.get(self.url, {'recent': 13, 'page': 2})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
         data = response.data
-        self.assertEqual(data[0]['username'], 'testuser3')
-        self.assertEqual(data[1]['username'], 'testuser2')
-        self.assertEqual(data[2]['username'], 'testuser1')
+        self.assertEqual(len(data), 3)
 
-    def test_get_recent_users_invalid_parameters(self):
+    def test_get_most_starred_repositories_invalid_parameters(self):
         response = self.client.get(self.url, {'recent': 'invalid', 'page': 'invalid'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], 'Invalid parameters')
 
-    def test_get_recent_users_no_users(self):
-        User.objects.all().delete()
+    def test_get_most_starred_repositories_no_repositories(self):
+        Repository.objects.all().delete()
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
 
     def test_unexpected_error_handling(self):
-        # Simulate an unexpected error by patching the get_recent_users method
+        # Simulate an unexpected error by patching the get_most_starred_repositories method
         with self.assertRaises(Exception):
             response = self.client.get(self.url)
             self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
---
+++ backend/RepoData/tests/test_user_view.py
@@ -0,0 +1,56 @@
+from django.test import TestCase, Client
+from ..models import *
+from rest_framework import status
+
+class UserViewTest(TestCase):
+
+
+    def setUp(self):
+        User.objects.create(username='testuser1')
+        User.objects.create(username='testuser2')
+        User.objects.create(username='testuser3')
+        self.client = Client()
+        self.url = "http://127.0.0.1:8000/v1/users/"
+
+    def test_get_recent_users_success(self):
+        response = self.client.get(self.url, {'recent': 2, 'page': 1})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        data = response.data
+        self.assertEqual(data[0]['username'], 'testuser3')
+        self.assertEqual(data[1]['username'], 'testuser2')
+
+    def test_get_recent_users_default(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_get_recent_users_success_pagination(self):
+        for i in range(4, 14):
+            User.objects.create(username='testuser' + str(i))
+
+        response = self.client.get(self.url, {'recent': 13, 'page': 2})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        data = response.data
+        self.assertEqual(data[0]['username'], 'testuser3')
+        self.assertEqual(data[1]['username'], 'testuser2')
+        self.assertEqual(data[2]['username'], 'testuser1')
+
+    def test_get_recent_users_invalid_parameters(self):
+        response = self.client.get(self.url, {'recent': 'invalid', 'page': 'invalid'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['error'], 'Invalid parameters')
+
+    def test_get_recent_users_no_users(self):
+        User.objects.all().delete()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_unexpected_error_handling(self):
+        # Simulate an unexpected error by patching the get_recent_users method
+        with self.assertRaises(Exception):
+            response = self.client.get(self.url)
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['error'], "An unexpected error occurred.")
